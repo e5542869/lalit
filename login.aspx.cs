39c393ab71e21446d7021ac6290febc39a30854b
e5542869 <50436325+e5542869@users.noreply.github.com>
2019-05-17 17:34:07 +0530

    Add files via upload

---
+++ login.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+public partial class login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (TextBox1.Text == "abc" && TextBox2.Text == "xyz")
+        {
+            FormsAuthentication.RedirectFromLoginPage(TextBox1.Text, false);
+        }
+        else {
+            Response.Write("Wrong user/pwd");
+        }
+    }
+}
